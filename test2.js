c7c0482e2f41ed3cb9513a5adc4193dc84ad52d9
Kingcheetah-135 <145504534+Kingcheetah-135@users.noreply.github.com>
2024-09-27 18:48:03 -0400

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,6 +1,6 @@
 //import test from "./test.js";
 export default function (x)
 {
-  fetch("./test.json").then((r) => { return r.json() }).then((r) => { console.log(r); })
+  fetch("https://cdn.jsdelivr.net/gh/kingcheetah-135/stucco-@branch2/test.json").then((r) => { return r.json() }).then((r) => { console.log(r); })
   //console.log(test(x) * 12);
 }
