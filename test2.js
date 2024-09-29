397ac5330ce0daa256a2b23ed22f97ff9483ea89
Kingcheetah-135 <145504534+Kingcheetah-135@users.noreply.github.com>
2024-09-29 16:33:06 -0400

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,6 +1,8 @@
 //import test from "./test.js";
 export default function (x)
 {
-  fetch("./test.json").then((r) => { r.json() }).then((r) => { console.log(r); })
-  //console.log(test(x) * 12);
+  let t = import("./test.js");
+  let test = t.default;
+  //fetch("./test.json").then((r) => { r.json() }).then((r) => { console.log(r); })
+  console.log(test(x) * 12);
 }
